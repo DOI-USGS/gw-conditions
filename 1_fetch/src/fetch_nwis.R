83c455b0a777802a1eaf4bfcd73000c2e6da51c0
Platt <lplatt@usgs.gov>
2021-03-15 13:24:46 -0500

    use bounding box to fetch viz sites

--- 1_fetch/src/fetch_nwis.R
+++ 1_fetch/src/fetch_nwis.R
@@ -1,18 +1,25 @@
-#' Fetch all NWIS sites that have groundwater level data during the appropriate time period
-fetch_gw_sites <- function(start_date, end_date, param_cd){
+#' Fetch NWIS sites within the bounding box that have groundwater level data during the appropriate time period
+fetch_gw_sites <- function(target_name, start_date, end_date, bbox, param_cd){
   
-  hucs <- zeroPad(1:21, 2) # all hucs
+  # From NWIS services documentation: The product of the range of latitude range 
+  #   and longitude multiplied cannot exceed 25 degrees. So, we need to cycle through  
+  #   large bounding boxes by dividing into query cells. Using 5 x 5 cells
+  
+  query_bbox_cells <- prep_latlon_cells(bbox[c(1,3)], bbox[c(2,4)], deg_per_slice = 5)
+  n_cells <- nrow(query_bbox_cells)
   
   sites <- c()
-  for(huc in hucs){
-    message(sprintf("Trying HUC %s ...", huc))
+  for(cell in seq_len(n_cells)) {
+    # For CONUS, we expect some to fail with no data since we put a square grid on
+    # top of the whole country, which means some cells are only over the oceans.
+    message(sprintf("Trying cell %s of %s ...", cell, n_cells))
     sites <- tryCatch(
-      whatNWISdata(huc = huc, 
-                   service = "dv", 
-                   startDate = start_date,
-                   endDate = end_date,
-                   parameterCd = param_cd,
-                   statCd = "00003") %>%
+      whatNWISdata(bBox = extract_cell_bbox(query_bbox_cells, cell), 
+                    service = "dv", 
+                    startDate = start_date,
+                    endDate = end_date,
+                    parameterCd = param_cd,
+                    statCd = "00003") %>%
         pull(site_no) %>%
         unique(), 
       error = function(e) return()
@@ -20,6 +27,56 @@ fetch_gw_sites <- function(start_date, end_date, param_cd){
   }
   
   return(sites)
+  
+}
+
+#' Function to divide the requested bounding box into NWIS query-able cells
+prep_latlon_cells <- function(long_vals, lat_vals, deg_per_slice) {
+  
+  long_slice_vals <- divide_latlon(long_vals, 5, "x")
+  lat_slice_vals <- divide_latlon(lat_vals, 5, "y")
+  cells <- expand.grid(long_i = seq_len(nrow(long_slice_vals)), lat_i = seq_len(nrow(lat_slice_vals)))
+  
+  cell_vals <- tibble(
+    xmin = long_slice_vals$xmin[cells$long_i],
+    ymin = lat_slice_vals$ymin[cells$lat_i],
+    xmax = long_slice_vals$xmax[cells$long_i],
+    ymax = lat_slice_vals$ymax[cells$lat_i]
+  ) %>% 
+    rowwise() %>% 
+    mutate(bbox = list(c(xmin, ymin, xmax, ymax))) %>% 
+    ungroup() # Undo "by row"
+  
+  # helper code here if you want to visualize the query cells:
+  # par(mar = c(0,0,0,0))
+  # maps::map('usa')
+  # rect(cell_vals$xmin, cell_vals$ymin, cell_vals$xmax, cell_vals$ymax)
+  
+  return(cell_vals)  
+}
+
+#' Helper function applied to either the latitude OR longitude decimal 
+#' vectors that divides them into NWIS query-managable sizes
+divide_latlon <- function(vals, deg_per_slice, name = c("x", "y")) {
+  
+  n_slices <- ceiling(diff(vals)/deg_per_slice)
+  
+  min_val_slice <- min(vals) + (seq_len(n_slices)-1)*deg_per_slice
+  max_val_slice <- min(vals) + (seq_len(n_slices))*deg_per_slice
+  
+  # Correct last slice to be max latitude
+  max_val_slice <- ifelse(max_val_slice > max(vals), max(vals), max_val_slice)
+  
+  slices <- tibble(min_val_slice, max_val_slice)
+  names(slices) <- paste0(name, c("min", "max"))
+  
+  return(slices)
+}
+
+#' Helper function to pull out nested bbox vector from 
+#' the big table of bbox values per cell
+extract_cell_bbox <- function(bbox_df, cell_i) {
+  unlist(bbox_df$bbox[cell_i])
 }
 
 fetch_gw_site_info <- function(sites) {
