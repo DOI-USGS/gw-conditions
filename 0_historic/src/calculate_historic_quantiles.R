2533a12603fdd3ba805570eeedc66ee38314d0c5
Platt <lplatt@usgs.gov>
2021-06-28 10:21:24 -0500

    add KS & FL to historic data

--- 0_historic/src/calculate_historic_quantiles.R
+++ 0_historic/src/calculate_historic_quantiles.R
@@ -4,16 +4,20 @@ combine_gw_uv_and_dv <- function(out_file, dv_fn, uv_fn) {
 }
 
 # Per gage, calc quantiles using historic groundwater level data (no seasonality)
-calculate_historic_quantiles <- function(target_name, historic_gw_data_fn, quantiles_to_calc) {
-  
+# If the gage reports "depth below", then we need to negate the values so that 
+# low percentiles line up with less water and high percentiles with more water. 
+# See this comment on GitHub for more detail/background: 
+#   https://github.com/USGS-VIZLAB/gw-conditions/issues/9#issuecomment-854115170
+
+calculate_historic_quantiles <- function(target_name, historic_gw_data_fn, quantiles_to_calc, inverse_sites) {
   read_csv(historic_gw_data_fn, col_types = 'cDn') %>% 
+    mutate(is_inverse = site_no %in% inverse_sites) %>% 
     split(.$site_no) %>% 
     map_dfr(., function(.x) {
-      quants <- quantile(.x$GWL, quantiles_to_calc, na.rm = TRUE)
+      quants <- quantile(.x$GWL*ifelse(.x$is_inverse, -1, 1), quantiles_to_calc, na.rm = TRUE)
       names(quants) <- NULL # Otherwise, carries unnecessary metadata
       tibble(quantile_nm = quantiles_to_calc*100, quantile_va = quants)
     }, .id = "site_no") %>% 
     bind_rows() %>% 
     write_csv(target_name)
-  
 }
--- 0_historic/src/filter_historic_fetched_data.R
+++ 0_historic/src/filter_historic_fetched_data.R
@@ -9,12 +9,8 @@ filter_historic_fetched_data <- function(target_name, historic_gw_data_fn, min_y
     filter(n_days >= 365*min_years) %>% 
     pull(site_no)
   
-  # 6/4/2021: One of the sites had its leading zero dropped along the way. I  
-  # know that it is a LK site, which we are not keeping, so going to manually 
-  # filter it out for now and return to this the next time we go to rebuild.
   site_nums <- unique(historic_gw_data$site_no)
-  i_bad <- which(site_nums %in% c("5346050")) # It should be "05346050"
-  sites_meet_site_tp <- readNWISsite(site_nums[-i_bad]) %>% 
+  sites_meet_site_tp <- readNWISsite(site_nums) %>% 
     select(site_no, site_tp_cd) %>% 
     filter(site_tp_cd %in% allowed_site_types) %>% 
     pull(site_no)
@@ -23,7 +19,7 @@ filter_historic_fetched_data <- function(target_name, historic_gw_data_fn, min_y
   historic_gw_data %>% 
     filter(site_no %in% sites_meet_min_yrs) %>% 
     filter(site_no %in% sites_meet_site_tp) %>%
-    write_csv(target_name) 
+    write_csv(file=target_name) 
 }
 
 gather_metadata_of_sites_used <- function(target_name, site_data_filtered_fn, site_data_service_to_pull, site_metadata) {
