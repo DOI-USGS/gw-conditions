7ba5621c8c094d8f71b1b0d366ce494f2f8fcf74
collnell <cnell@usgs.gov>
2021-07-07 11:50:05 -0500

    define peaks with defs

--- src/components/GWL.vue
+++ src/components/GWL.vue
@@ -2,6 +2,9 @@
   <section>
     <div id="title-container">
       <h1>U.S. Groundwater conditions</h1></div>
+    <div>
+
+</div>
     <div id="map-container">
        <GWLmap class="map" />
     </div>
@@ -9,7 +12,7 @@
 </template>
 <script>
 import * as d3Base from 'd3';
-import GWLmap from "@/assets/anomaly_peaks.svg";
+import GWLmap from "@/assets/anomaly_peaks_20201031_20201031.svg";
 export default {
   name: "GWLsvg",
     components: {
@@ -17,6 +20,10 @@ export default {
     },
     data() {
     return {
+      publicPath: process.env.BASE_URL, // this is need for the data files in the public folder, this allows the application to find the files when on different deployment roots
+      d3: null,
+      quant_peaks: null,
+      date_peaks: null,
 
     }
   },
@@ -24,29 +31,50 @@ export default {
 
       this.d3 = Object.assign(d3Base);
       this.colorStroke();
-      this.getSVGs();
-      
+      this.loadData();      
     },
     methods:{
+       loadData() {
+        const self = this;
+        // read in data 
+        let promises = [self.d3.csv(self.publicPath + "svg_files.csv",  this.d3.autotype),
+        self.d3.csv(self.publicPath + "quant_peaks.csv",  this.d3.autotype),
+        self.d3.csv(self.publicPath + "date_peaks.csv",  this.d3.autotype)];
+        Promise.all(promises).then(self.callback); 
+      },
+      callback(data) {
+        var svg_files = data[0];
+        this.quant_peaks = data[1];
+        this.date_peaks = data[2];
+
+        this.makePeaks();
+      },
+      makePeaks(){
+        var svg = this.d3.select("svg")
+        var peak_defs = svg.append('defs').append('g').attr("id","peaks")
+
+        // add defs for peak categories and <use> with attrTweeb
+        peak_defs.selectAll("path").data(this.quant_peaks).enter()
+          .append("path")
+          .attr("fill", function(d){return d["color"]})
+          .attr("d", function(d){return d["path_quant"]})
+          .attr("id", function(d){return d["quant"]})
+
+          // test how to draw using <use>
+          svg.append("g").attr("transform","translate(250,200) scale(2,2)")
+		        .append("use").attr("xlink:href","#High").attr("class","peak")
+
+          // for every site, draw a peak for the first day of the time period
+          // requires input data with site, site coords in svg, date, and quant_category
+
+      },
       colorStroke(){
 
         this.d3.selectAll(".map-bkgrd") // map outline
           .attr("fill", "transparent")
           .attr("stroke", "black")
           .attr("stroke-width", "1px")
-          .transition()
-          .duration(2000)
-          .attr("stroke", "pink")
-          .transition()
-          .duration(2000)
-          .attr("stroke", "purple")
-          .transition()
-          .duration(2000)
-          .attr("stroke", "blue")
-          .transition()
-          .duration(2000)
-          .attr("stroke", "green")
-          .on("end", this.rainbowStrobe);
+          
 
       },
       rainbowStrobe(){
@@ -64,13 +92,6 @@ export default {
           .duration(2000)
           .attr("stroke", "orchid")
           .on("end", this.colorStroke);
-      },
-      getSVGs(){
-        const svg_list = require.context(
-          '@/assets',
-          true,/^.*\.svg$/
-        )
-        console.log(svg_list)
       }
     }
 }
@@ -84,9 +105,10 @@ export default {
 #title-container {
   width: 100vw;
   height: auto;
-  background-color: #c2c4c5;
+  background-color: #0d5fca;
   h1 {
     width: 95vw;
+    color:white;
     height: auto;
     padding: 15px;
     
--- src/main.js
+++ src/main.js
@@ -14,7 +14,6 @@ import VueCarousel from 'vue-carousel';
 import VueImg from 'v-img';
 import VueSvg from 'vue-svg-loader';
 
-
 // social icons
 import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
@@ -22,7 +21,6 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faFlickr } from "@fortawesome/free-brands-svg-icons";
 import { faYoutubeSquare } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-
 
 const vueImgConfig = {
   altAsTitle: true
@@ -43,7 +41,8 @@ Vue.use(uswds);
 Vue.use(browserDetect);
 Vue.use(Vuetify);
 Vue.use(VueCarousel);
-Vue.use(VueImg, vueImgConfig)
+Vue.use(VueImg, vueImgConfig);
+Vue.use(VueSvg);
 
 const app = new Vue({
   router,
