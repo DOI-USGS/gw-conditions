9205b5b4cb4172b84a3af89428f78edbfbb76207
collnell <cnell@usgs.gov>
2021-12-29 14:34:41 -0600

    add aria labels to svg in pipeline

--- 3_visualize/src/build_peaks_svg.R
+++ 3_visualize/src/build_peaks_svg.R
@@ -1,7 +1,8 @@
 
-build_peaks_svg <- function(out_file, svg_width, svg_height, digits = 6) {
+build_peaks_svg <- function(out_file, svg_width, svg_height, digits = 6, aria_title, aria_desc) {
   
-  svg_root <- init_svg(viewbox_dims = c(0, 0, svg_width=svg_width, svg_height=svg_height))
+  svg_root <- init_svg(viewbox_dims = c(0, 0, svg_width=svg_width, svg_height=svg_height),
+                       aria_title, aria_desc)
   
   add_background_map(svg_root, svg_width = svg_width, outline_states = FALSE, digits)
   
